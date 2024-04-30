30e93d7aacbb40727a52e3b3b0e9b73b75950ea4
Shivam Kumar <shivam.kumar@kraken.tech>
2024-04-30 14:53:41 +0100

    Add scripts folder

---
+++ scripts/python_part_converter/python_path_converter.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+  "os"
+	"strings"
+)
+
+func fromImportConverter(input string) string {
+	split := strings.Fields(input)
+	return split[1] + "." + split[len(split)-1]
+}
+
+func dotPathConverter(input string) string {
+	split := strings.Split(input, ".")
+	return strings.Join(split[:len(split)-1], "/")
+}
+
+func main() {
+  query := os.Args[1]
+	var pathOut string
+
+	if strings.Fields(query)[0] == "from" {
+		pathOut = fromImportConverter(query)
+	} else {
+		pathOut = dotPathConverter(query)
+	}
+
+	fmt.Println(pathOut)
+}
