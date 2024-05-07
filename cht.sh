3c4bf190e77c6653a22db007dd43fe10d23b71e4
Shivam Kumar <shivam.kumar@kraken.tech>
2024-05-07 09:58:58 +0100

    Add cht.sh

---
+++ cht.sh
@@ -0,0 +1,19 @@
+# ~/usr/bin/env bash
+
+languages=$(echo "python lua golang" | tr ' ' '\n')
+core_utils=$(echo "xargs find mv sed awk" | tr ' ' '\n')
+
+selected=$(printf "$languages\n$core_utils" | fzf)
+
+if [[ -z $selected ]]; then
+	exit 0
+fi
+
+read -p "Enter Query: " query
+
+if printf $languages | grep -qs $selected; then
+	query=$(echo $query | tr ' ' '+')
+	tmux neww bash -c "curl -s cht.sh/$selected/$query | bat -p | less"
+else
+	tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+fi
