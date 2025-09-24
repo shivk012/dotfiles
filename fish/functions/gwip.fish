16405915b377bdb523bab57ce8a1c259d1d5475b
Shivam Kumar <shivam.kumar@kraken.tech>
2025-09-24 18:37:06 +0100

    Add gwip to fish

--- fish/functions/gwip.fish
+++ fish/functions/gwip.fish
@@ -3,5 +3,7 @@
 # When you want to go back to work, just unwip it
 #
 function gwip -d "git commit a work-in-progress branch"
-  git add -A; git rm (git ls-files --deleted) 2> /dev/null; git commit -m "--wip--" --no-verify
+    git add -A
+    git rm (git ls-files --deleted) 2>/dev/null
+    git commit -m "--wip-- [skip-ci]" --no-verify
 end
