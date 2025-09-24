5dabdf251136bac4754e152e15fc52040e86e241
Shivam Kumar <shivam.kumar@kraken.tech>
2025-09-24 18:37:19 +0100

    Add some git plugins and learning plugins

--- nvim/lua/plugins/kraken.lua
+++ nvim/lua/plugins/kraken.lua
@@ -3,5 +3,6 @@ return {
   opts = {},
   keys = {
     { "<leader>kk", "<cmd>KLocateTests<cr>", desc = "Kraken locate tests" },
+    { "<leader>kp", "<cmd>KCreateSlackPrLink<cr>", desc = "Kraken copy PR link" },
   },
 }
