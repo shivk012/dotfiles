bf20d767296f1d4d632690e213097eb1417ab584
Shivam Kumar <shivam_k@hotmail.com>
2026-03-28 12:28:47 +0000

    personal update

--- nvim/lua/plugins/kraken.lua
+++ nvim/lua/plugins/kraken.lua
@@ -5,4 +5,5 @@ return {
     { "<leader>kk", "<cmd>KLocateTests<cr>", desc = "Kraken locate tests" },
     { "<leader>kp", "<cmd>KCreateSlackPrLink<cr>", desc = "Kraken copy PR link" },
   },
+  cond = string.find((os.getenv("HOSTNAME") or ""):lower(), "kraken") ~= nil,
 }
